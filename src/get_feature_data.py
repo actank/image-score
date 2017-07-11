70bfeaab51f84d2f12b40d818cfafae4af04780b
yujishi <yujishi@higohappy.com>
2017-07-11 19:30:49 +0800

    MOD:update

---
+++ src/get_feature_data.py
@@ -0,0 +1,19 @@
+#coding:utf-
+
+import sys
+import os
+
+
+def get_positive_data():
+    return
+
+def get_negative_data():
+    return
+
+def main():
+    get_positive_data()
+    get_negative_data()
+    return
+
+if __name__ == "__main__":
+    main()
